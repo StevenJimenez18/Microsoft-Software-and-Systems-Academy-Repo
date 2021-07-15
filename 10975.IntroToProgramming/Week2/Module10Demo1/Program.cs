cc73d35bf4feb897760a9066347d7465a8bea1a4
Steven Jimenez <steven.jimenez2029@gmail.com>
2021-07-15 13:06:26 -0400

    Module10Demo1: Added Boxing and Unboxing examples

--- 10975.IntroToProgramming/Week2/Module10Demo1/Program.cs
+++ 10975.IntroToProgramming/Week2/Module10Demo1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Module10Demo1
 {
@@ -14,8 +15,22 @@ namespace Module10Demo1
             int num1, num2;
             num1 = 100;
             num2 = 200;
-            Swap(ref num1,ref num2);
+            Swap(ref num1,ref num2); //Passing by reference
             Console.WriteLine($"Swapped Values: num1: {num1}, num2 {num2}");
+
+
+            //Boxing 
+            object o = num1; //value conversion to ref type... implicit 
+
+            //Unboxing
+            int i = (int)o;  // ref conversion to value type... explicit 
+
+
+            //Example
+            Stack st = new Stack();
+            st.Push(num1); //Boxing
+            int num3 = (int)st.Pop(); //UnBoxing
+
             Console.ReadLine();
 
         }
