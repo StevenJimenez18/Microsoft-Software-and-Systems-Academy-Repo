c080a43f581e719f7583652befb1af14cf3f00c3
Steven Jimenez <steven.jimenez2029@gmail.com>
2021-07-15 12:46:22 -0400

    Added Module10 Demo 1 for reference type passing practice

---
+++ 10975.IntroToProgramming/Week2/Module10Demo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2;
+            num1 = 100;
+            num2 = 200;
+            Swap(ref num1,ref num2);
+            Console.WriteLine($"Swapped Values: num1: {num1}, num2 {num2}");
+            Console.ReadLine();
+
+        }
+
+        static void Swap(ref int i,ref int j)
+        {
+            int temp;
+            temp = i;
+            i = j;
+            j = temp;
+        }
+    }
+}
